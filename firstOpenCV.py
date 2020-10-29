18f33521f468039938580a3a054142e33a7aa0eb
Witaya252525 <witaya.c@kkumail.com>
2020-10-30 00:48:28 +0700

    first opencv

---
+++ firstOpenCV.py
@@ -0,0 +1,6 @@
+git 
+import cv2
+img =cv2.imread('./image/lena.jpg')
+cv2.imshow("image" ,img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- witaya.py
+++ witaya.py
@@ -1,5 +1,41 @@
 
+# print("chaison")
+# weight = 60
+# Height =1.7
+# BMI = weight / (Height **2)
+# print(" Your Mass Body is" , BMI)
+
+# w = input ("Weight is kg = ")
+# h = input ('Height is CM = ')
+# BMI = float(w) / (float(h) * float(h))
+# print ("The BMI is =  %.2f" %BMI)
 
 
+# for i in range(1,4):
+#     password = input("Enter Password = ")
+#     if password == "1234" :
+#         print("start your transition")
+#         break
 
-print("chaison")
+#     if i == 3:
+#         print ("contact you bank accout")    
+
+# i=0
+# while True :
+#     password = input("Enter Password = ")
+#     if password == "1234" :
+#         print("start your transition")
+#         break
+#     i = i+1
+#     if i == 3 :
+#         print ("contact you bank accout")  
+#         i = 0 
+#         break 
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+print("Date is :" , x.strftime("%d/%m/%Y"))
+print("Time is :" , x.strftime("%H/%M/%S"))
+
